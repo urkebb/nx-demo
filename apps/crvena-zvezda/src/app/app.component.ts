6a2d7182cfdcc3f48b451f60590fbb006e798cdc
Uros Milic <uros.milic@itcentar.rs>
2022-01-10 15:35:26 +0100

    initial commit

---
+++ apps/crvena-zvezda/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'viber-bot-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'crvena-zvezda';
+}
